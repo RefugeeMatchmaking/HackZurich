6d49b129b62e4e326a4f6178fefca14fda2b0952
sasha42 <sasha42@users.noreply.github.com>
2015-10-03 09:38:25 +0200

    Fixed users model and admin titles

--- refugee_matchmaking/users/models.py
+++ refugee_matchmaking/users/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 
 class Language(models.Model):
 	language = models.CharField(max_length=100)
+	def __str__(self):
+		return self.language
 
 class User(models.Model):
     GENDER = (
@@ -14,7 +16,7 @@ class User(models.Model):
         ('0', 'Refugee'),
         ('1', 'Local'),
     )
-    refugee_or_local = models.IntegerField(choices=STATUS)
+    refugee_or_local = models.CharField(choices=STATUS, max_length=1)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
@@ -23,9 +25,11 @@ class User(models.Model):
     picture = models.ImageField(blank=True)
     email = models.EmailField(max_length=254)
     langauge = models.ManyToManyField(Language)
-    gender = models.IntegerField(choices=GENDER)
-    gender_preference = models.PositiveIntegerField(blank=True, choices=GENDER)
+    gender = models.CharField(choices=GENDER, max_length=1)
+    gender_preference = models.CharField(blank=True, choices=GENDER, max_length=1)
     birthdate = models.DateField()
     social_media = models.URLField(max_length=200, blank=True)
     submitted  = models.DateTimeField(default=timezone.now, editable=False)
     submission_ip = models.GenericIPAddressField(protocol='both')
+    def __str__(self):
+        return self.last_name
