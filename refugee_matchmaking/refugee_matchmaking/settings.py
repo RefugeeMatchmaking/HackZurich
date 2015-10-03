f57e896344ccd9aaeef641099f48a6408a86d2bf
sasha42 <sasha42@users.noreply.github.com>
2015-10-03 09:53:56 +0200

    Added django-grappelli for nicer admin interface

--- refugee_matchmaking/refugee_matchmaking/settings.py
+++ refugee_matchmaking/refugee_matchmaking/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 "django.core.context_processors.request",
             ],
         },
     },
