5084affb9655f281007dd451ba12fd1a9feb8233
Vojta Havlicek <vojta.havlicek@gmail.com>
2015-10-03 19:33:15 +0200

    pre-pull

--- get_score.py
+++ get_score.py
@@ -5,11 +5,11 @@ def get_score(User1, User2):
 	matchrank = 0
 
 	#check if one if host and one is refugee
-	if User1.staus == User2.status:
+	if User1.status == User2.status:
 		return -1 # both hosts or refugees
 
 	#by location
-	matchrank = location_rank(User1.location, User2.location, matchrank)
+	#matchrank = location_rank(User1.location, User2.location, matchrank)
 
 	#check if zero
 	if matchrank == 0:
@@ -19,33 +19,28 @@ def get_score(User1, User2):
 
 
 	#by gender preference
-	matchrank = gender_preference_rank(User1.Gender, User2.Gender, User1.Gender_Pref, User2.Gender_Pref, matchrank)
+	matchrank = gender_preference_rank(User1.gender, User2.gender, User1.gender_pref, User2.gender_pref, matchrank)
 
 	# by age
-	matchrank = age_rank(User1.DOB, User2.DOB, matchrank)
+	""" Disabled for now """
+	matchrank = age_rank(User1.dob, User2.dob, matchrank)
 	if matchrank == 0:
 		print('Nogo criteria found. No match!')
 
 		return -1
 
 	#check if zero
-	if matchrank == 0:
-		print('Nogo criteria found. No match!')
+	#if matchrank == 0:
+	#	print('Nogo criteria found. No match!')
 
-		return -1
+	#	return -1
 
-	matchrank = location_rank(User1.Languages, User2.Languages, matchrank)
-	if matchrank == 0:
-		print('Nogo criteria found. No match!')
+	""" Ignore for now """
+	#matchrank = location_rank(User1.languages, User2.languages, matchrank)
+	#if matchrank == 0:
+	#	print('Nogo criteria found. No match!')
 
-		return -1
+	#	return -1
 	#by age
 	
-
-
-
 	return matchrank
-
-
-
-	
