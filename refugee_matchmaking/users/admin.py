96f9b7ee9f796c757c5526fd00fd4f5c2402cc60
sasha42 <sasha42@users.noreply.github.com>
2015-10-03 15:51:28 +0200

    KISS: reverting back failed experiments

--- refugee_matchmaking/users/admin.py
+++ refugee_matchmaking/users/admin.py
@@ -1,28 +1,6 @@
 from django.contrib import admin
+
 from .models import User, Language
 
-class LanguageInline(admin.TabularInline):
-	model = Language
-	extra = 1
-
-class UserAdmin(admin.ModelAdmin):
-	readonly_fields = ("submitted", "submission_ip", "email")
-	date_hierarchy = 'submitted'
-	fieldsets = (
-		('User info', {
-			'classes': ('grp-collapse grp-open',),
-			'fields': ('refugee_or_local', 'first_name', 'last_name', 'location')
-		}),
-		('Details', {
-			'classes': ('grp-collapse grp-closed',),
-			'fields': ('about', 'occupation', 'gender', 'gender_preference', 'social_media')
-		}),
-		('Meta', {
-			'classes': ('grp-collapse grp-closed',),
-			'fields': ('submitted', 'submission_ip', 'email', 'picture')
-		}),
-	)
-	inlines = [LanguageInline]
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
 admin.site.register(Language)
