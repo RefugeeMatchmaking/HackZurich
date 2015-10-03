2806cd1aeace6b52040856a74872f16658a087e2
sasha42 <sasha42@users.noreply.github.com>
2015-10-03 11:52:05 +0200

    Fix languages

--- refugee_matchmaking/refugee_matchmaking/settings.py
+++ refugee_matchmaking/refugee_matchmaking/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'grappelli_nested',
     'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -103,3 +104,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_DIR = os.path.dirname(__file__)
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+GRAPPELLI_ADMIN_TITLE = 'Refugee Matchmaking'
