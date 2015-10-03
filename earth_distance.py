1cbb66ff22f2c505d66b8cd2eedc5b8e53877cfc
Bernhard <b.vennemann@gmail.com>
2015-10-03 19:40:23 +0200

    new version of earth_distance

--- earth_distance.py
+++ earth_distance.py
@@ -5,18 +5,24 @@ from urllib.request import urlopen
 def get_earth_distance(loc1, loc2):
 	'''Function that returns the driving distance between two locations'''
 	
-	url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(loc1),str(loc2))
+	#url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(loc1),str(loc2))
 	
-	result= simplejson.load(urlopen(url))
+	#result= simplejson.load(urlopen(url))
 	
-	print(result)
-	driving_distance = result['rows'][0]['elements'][0]['distance']['value']/1000 # [km]
+	#print(result)
+	#driving_distance = result['rows'][0]['elements'][0]['distance']['value']/1000 # [km]
 	
-	return driving_distance
+	lat1, lng1 = lat_long(loc1)
+	lat2, lng2 = lat_long(loc2)
+
+	
+
+
+	return earth_distance
 
 
 
 if __name__ == "__main__":
-	driving_distance = get_earth_distance("koeln","bonn")
-	print(driving_distance)
+	earth_distance = get_earth_distance("koeln","bonn")
+	print(earth_distance)
 	
