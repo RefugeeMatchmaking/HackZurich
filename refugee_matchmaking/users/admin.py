b72a3ec5a0019c715a6fa29bd949a37697ea314b
Prageeth Jayathissa <jayathip@ita-schlueter-m.local>
2015-10-19 01:04:07 +0200

    gave admin a list_display for users

--- refugee_matchmaking/users/admin.py
+++ refugee_matchmaking/users/admin.py
@@ -6,9 +6,12 @@ class LanguageInline(admin.TabularInline):
 	extra = 1
 
 class UserAdmin(admin.ModelAdmin):
-	readonly_fields=('submitted', 'submission_ip',)
+	readonly_fields=('submitted', 'submission_ip')
 	model = User
 	inlines = [LanguageInline]
+	list_display = ('first_name', 'location', 'gender','latitude_longitude')
+
+	#Add some function which cacluates the logitude and lattitude when the admin inputs the data
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Language)
