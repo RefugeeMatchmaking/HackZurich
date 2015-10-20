816552d7226514b2d663986d370b2e834fbc3b35
Bernhard <b.vennemann@gmail.com>
2015-10-20 17:07:54 +0200

    small bugfix in forms.py (removed picture entry)

--- refugee_matchmaking/users/forms.py
+++ refugee_matchmaking/users/forms.py
@@ -5,4 +5,5 @@ class UserForm(forms.ModelForm):
 
 	class Meta:
 		model = User
-		fields = ['refugee_or_local', 'first_name', 'last_name', 'location', 'occupation', 'about', 'picture', 'email', 'gender', 'gender_preference', 'birthdate', 'social_media']
+		#fields = ['refugee_or_local', 'first_name', 'last_name', 'location', 'occupation', 'about', 'picture', 'email', 'gender', 'gender_preference', 'birthdate', 'social_media']
+		fields = ['refugee_or_local', 'first_name', 'last_name', 'location', 'occupation', 'about', 'email', 'gender', 'gender_preference', 'birthdate', 'social_media']
