30cc4bd76eb202468bd729518308fd4889dd22bb
Vojta Havlicek <vojta.havlicek@gmail.com>
2015-11-21 18:19:53 +0100

    algorithm changed

--- refugee_matchmaking/users/algorithms/get_square_V2.py
+++ refugee_matchmaking/users/algorithms/get_square_V2.py
@@ -30,7 +30,7 @@ def get_square(node,all_locals,all_refugees):
 
 		s1 = get_score(node,friend)
 
-		if s1>threshold: #If the score is higher than the threshold then save it
+		if s1 > threshold: #If the score is higher than the threshold then save it
 
 			#Now we want to loop through the second database and compare that to our newly found friend
 			for friend_of_friend in scnd:
@@ -38,24 +38,30 @@ def get_square(node,all_locals,all_refugees):
 				if friend_of_friend is node:
 					continue # skip itself
 
-				s2=get_score(friend,friend_of_friend)
+				s2 = get_score(friend,friend_of_friend)
 
-				if s2>threshold: #If the score is higher than the threshold then we have found a triangluar match (3 people)
+				if s2 > threshold: #If the score is higher than the threshold then we have found a triangluar match (3 people)
+					
+					s3 = get_score(node, friend_of_friend)
 					if friend_of_friend in triangle:
-						triangle[friend_of_friend].append((friend,s1+s2))
+						triangle[friend_of_friend].append((friend,2*s1+2*s2 + s3 ))
 					else:
-						triangle[friend_of_friend]=[(friend,s1+s2)]
+						triangle[friend_of_friend]=[(friend,2*s1+2*s2+s3)]
 				
 
 	#Loop through the dictionary of triangles
 	for fof in triangle:
 
-		#If our friend_of_friend has more two friends or more then we have a square
+		#If our friend_of_friend connects back to node via another refugee, we get a square
 		if len(triangle[fof])>1:
 			#If our friend_of_friend has more than two friends then we need to obtain the top two
 			toptwo=sorted(triangle[fof], key=lambda tup: tup[1])[:2]
-			#Lets sum the scores of the top two friends
-			new_score = toptwo[0][1] + toptwo[1][1]
+			#Lets sum the scores of the top two friends and add the scores between the friends
+			left_friend = toptwo[0][0]
+			right_friend = toptwo[1][0]
+			s4 = 2*get_score(left_friend, right_friend)
+
+			new_score = toptwo[0][1] + toptwo[1][1] + s4 # this gets the score as a sum of all edges in fully connected 4 point graph
 			#if this score is the highest, then save the square, if not, leave it
 			if new_score>highest:
 				square=[node,toptwo[0][0],toptwo[1][0], fof]
