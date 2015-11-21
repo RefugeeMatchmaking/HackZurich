0ce6ba4a822abb9556e99a84a9e08bab52b6e0fe
Prageeth Jayathissa <p.jayathissa@gmail.com>
2015-11-21 20:09:14 +0100

    added datepicker

--- refugee_matchmaking/users/forms.py
+++ refugee_matchmaking/users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import User
+from .models import User, Language
+from django.contrib.admin import widgets  
 
 class UserForm(forms.ModelForm):
 
@@ -7,3 +8,11 @@ class UserForm(forms.ModelForm):
 		model = User
 		#fields = ['refugee_or_local', 'first_name', 'last_name', 'location', 'occupation', 'about', 'picture', 'email', 'gender', 'gender_preference', 'birthdate', 'social_media']
 		fields = ['refugee_or_local', 'first_name', 'last_name', 'location', 'occupation', 'about', 'email', 'gender', 'gender_preference', 'birthdate', 'social_media']
+		help_texts = {
+            'birthdate': ('eg: year-month-day'),
+            }
+		widgets = {
+            'birthdate': forms.DateInput(attrs={'class':'datepicker'}),
+        }
+
+
